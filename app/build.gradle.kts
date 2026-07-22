d6046a755a76344c34dd586fa8d9f0247e4e7a0f
Djihads80 <mahboubijoujou111@gmail.com>
2026-07-22 13:56:36 +0100

    build fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,12 +18,12 @@ val localProperties = Properties().apply {
 
 android {
     namespace = "com.djihad.shopnite"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.djihad.shopnite"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
