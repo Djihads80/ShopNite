5e287cb96beccef80f1849b18fdd73f5fef48175
Djihads80 <mahboubijoujou111@gmail.com>
2026-07-23 12:39:08 +0100

    Syntax fix

--- app/src/main/java/com/djihad/shopnite/ui/components/CommonComponents.kt
+++ app/src/main/java/com/djihad/shopnite/ui/components/CommonComponents.kt
@@ -1,5 +1,6 @@
 package com.djihad.shopnite.ui.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
