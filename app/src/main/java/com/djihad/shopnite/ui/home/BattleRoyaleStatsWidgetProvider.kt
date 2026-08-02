c19ea455e5e7cb0b6dfb20b18facca8ac31cf3aa
Djihads80 <mahboubijoujou111@gmail.com>
2026-08-02 15:00:32 +0100

    more widget tings

--- app/src/main/java/com/djihad/shopnite/ui/home/BattleRoyaleStatsWidgetProvider.kt
+++ app/src/main/java/com/djihad/shopnite/ui/home/BattleRoyaleStatsWidgetProvider.kt
@@ -93,16 +93,19 @@ class BattleRoyaleStatsWidgetProvider : AppWidgetProvider() {
         setTextViewText(R.id.widget_subtitle, summary?.let { context.getString(R.string.widget_battle_royale_subtitle) } ?: context.getString(R.string.widget_battle_royale_setup))
 
         setInt(R.id.widget_root, "setBackgroundResource", R.drawable.widget_background)
-        setInt(R.id.widget_header, "setBackgroundColor", Color.parseColor("#6750A4"))
-        setInt(R.id.widget_stat_card_1, "setBackgroundColor", Color.parseColor("#E8DEF8"))
-        setInt(R.id.widget_stat_card_2, "setBackgroundColor", Color.parseColor("#E8DEF8"))
-        setInt(R.id.widget_stat_card_3, "setBackgroundColor", Color.parseColor("#E8DEF8"))
-        setInt(R.id.widget_stat_card_4, "setBackgroundColor", Color.parseColor("#E8DEF8"))
-        setInt(R.id.widget_stat_card_5, "setBackgroundColor", Color.parseColor("#E8DEF8"))
-        setInt(R.id.widget_stat_card_6, "setBackgroundColor", Color.parseColor("#E8DEF8"))
+        setInt(R.id.widget_header, "setBackgroundResource", R.drawable.widget_header_bg)
+        setInt(R.id.widget_stat_card_1, "setBackgroundResource", R.drawable.widget_stat_card_bg)
+        setInt(R.id.widget_stat_card_2, "setBackgroundResource", R.drawable.widget_stat_card_bg)
+        setInt(R.id.widget_stat_card_3, "setBackgroundResource", R.drawable.widget_stat_card_bg)
+        setInt(R.id.widget_stat_card_4, "setBackgroundResource", R.drawable.widget_stat_card_bg)
+        setInt(R.id.widget_stat_card_5, "setBackgroundResource", R.drawable.widget_stat_card_bg)
+        setInt(R.id.widget_stat_card_6, "setBackgroundResource", R.drawable.widget_stat_card_bg)
+        setInt(R.id.widget_stat_card_7, "setBackgroundResource", R.drawable.widget_stat_card_bg)
+        setInt(R.id.widget_stat_card_8, "setBackgroundResource", R.drawable.widget_stat_card_bg)
+        setInt(R.id.widget_stat_card_9, "setBackgroundResource", R.drawable.widget_stat_card_bg)
 
         val statTiles = summary?.statTiles.orEmpty()
-        val visibleStats = statTiles.take(if (statTiles.size >= 6) 6 else statTiles.size)
+        val visibleStats = statTiles.take(if (statTiles.size >= 9) 9 else statTiles.size)
 
         clearStatRows()
         visibleStats.forEachIndexed { index, stat ->
@@ -111,14 +114,17 @@ class BattleRoyaleStatsWidgetProvider : AppWidgetProvider() {
     }
 
     private fun RemoteViews.clearStatRows() {
-        for (index in 0 until 6) {
+        for (index in 0 until 9) {
             val labelId = when (index) {
                 0 -> R.id.widget_stat_1_label
                 1 -> R.id.widget_stat_2_label
                 2 -> R.id.widget_stat_3_label
                 3 -> R.id.widget_stat_4_label
                 4 -> R.id.widget_stat_5_label
-                else -> R.id.widget_stat_6_label
+                5 -> R.id.widget_stat_6_label
+                6 -> R.id.widget_stat_7_label
+                7 -> R.id.widget_stat_8_label
+                else -> R.id.widget_stat_9_label
             }
             val valueId = when (index) {
                 0 -> R.id.widget_stat_1_value
@@ -126,7 +132,10 @@ class BattleRoyaleStatsWidgetProvider : AppWidgetProvider() {
                 2 -> R.id.widget_stat_3_value
                 3 -> R.id.widget_stat_4_value
                 4 -> R.id.widget_stat_5_value
-                else -> R.id.widget_stat_6_value
+                5 -> R.id.widget_stat_6_value
+                6 -> R.id.widget_stat_7_value
+                7 -> R.id.widget_stat_8_value
+                else -> R.id.widget_stat_9_value
             }
             setTextViewText(labelId, "")
             setTextViewText(valueId, "")
@@ -140,7 +149,10 @@ class BattleRoyaleStatsWidgetProvider : AppWidgetProvider() {
             2 -> R.id.widget_stat_3_label
             3 -> R.id.widget_stat_4_label
             4 -> R.id.widget_stat_5_label
-            else -> R.id.widget_stat_6_label
+            5 -> R.id.widget_stat_6_label
+            6 -> R.id.widget_stat_7_label
+            7 -> R.id.widget_stat_8_label
+            else -> R.id.widget_stat_9_label
         }
         val valueId = when (index) {
             0 -> R.id.widget_stat_1_value
@@ -148,7 +160,10 @@ class BattleRoyaleStatsWidgetProvider : AppWidgetProvider() {
             2 -> R.id.widget_stat_3_value
             3 -> R.id.widget_stat_4_value
             4 -> R.id.widget_stat_5_value
-            else -> R.id.widget_stat_6_value
+            5 -> R.id.widget_stat_6_value
+            6 -> R.id.widget_stat_7_value
+            7 -> R.id.widget_stat_8_value
+            else -> R.id.widget_stat_9_value
         }
         setTextViewText(labelId, stat.label)
         setTextViewText(valueId, stat.value)
